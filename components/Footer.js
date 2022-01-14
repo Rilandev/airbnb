05030fb24ecddbb1b0d34db0b8e8dc5f8bc20ada
Rilandev <95538487+Rilandev@users.noreply.github.com>
2022-01-14 20:43:19 +0530

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>News room</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Link</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>Its a pretty awesome clone</p>
+                <p>Referels accepted</p>
+                <p>papafam</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>papa react</p>
+                <p>Presents</p>
+                <p>Zero Full stuck Hero</p>
+                <p>Hundreds of Students</p>
+                <p>Join Now</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>papa react</p>
+                <p>Presents</p>
+                <p>Zero Full stuck Hero</p>
+                <p>Hundreds of Students</p>
+                <p>Join Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
